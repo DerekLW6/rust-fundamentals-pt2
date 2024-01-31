bd155cc0d45ad0c5f1941158fb98a9efec162219
Derek <53124417+DerekLW6@users.noreply.github.com>
2024-01-31 10:58:57 +0000

    Sort of got the file reader

--- examples/16-error-handling/error-handling/src/main.rs
+++ examples/16-error-handling/error-handling/src/main.rs
@@ -1,29 +1,16 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::env;
 
 fn main() {
-    let file = File::open("non_existent_file.txt");
-    let file = match file {
-        Ok(file) => file,
-        Err(error) => {
-            match error.kind() {
-                std::io::ErrorKind::NotFound => {
-                    panic!("File not found: {}", error)
-                }
-                _ => {
-                    panic!("Error opening file: {}", error)
-                }
-            }
-        }
-    };
-    
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        match line {
-            Ok(line) => println!("{}", line),
-            Err(error) => {
-                panic!("Error reading line: {}", error)
-            }
-        }
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+
+    // Check if a file name was provided
+    if args.len() < 2 {
+        panic!("Please provide a file name as argument");
     }
+
+    let _file = File::open(&args[1]).unwrap();
 }
